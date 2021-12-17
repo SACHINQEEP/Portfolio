d7e70342c6f3df61a2c5887fa3bfecb4a6f41d16
SACHINQEEP <patelsachinsp269@gmail.com>
2021-12-18 02:11:50 +0530

    new-commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 'use strict';
 
 const toggle = document.querySelector('.toggle-bar');
-const navItems = document.querySelector('.nav-item');
+const navItems = document.querySelector('.nav-items');
 
 toggle.addEventListener('click', function (e) {
   e.preventDefault();
   navItems.classList.toggle('hidden');
+});
+
+// Smooth Scrolling
+
+const smooth = document.querySelector('.nav-links');
+
+smooth.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(e.target);
+
+  if (e.target.classList.contains('nav-link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
