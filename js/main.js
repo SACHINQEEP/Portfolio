662b488ee1272d483bdf6716ef4482f701b36ed4
SACHINQEEP <patelsachinsp269@gmail.com>
2021-12-21 22:49:35 +0530

    new-commit

--- js/main.js
+++ js/main.js
@@ -2,51 +2,34 @@
 
 const { async } = require('regenerator-runtime');
 
-const toggle = document.querySelector('.toggle-bar');
-const navItems = document.querySelector('.nav-links');
+const toogle = document.querySelector('.toogle');
+const navItems = document.querySelector('.nav-items');
+const animation = document.querySelector('nav');
 
-toggle.addEventListener('click', function (e) {
+toogle.addEventListener('click', function (e) {
   e.preventDefault();
-  navItems.classList.toggle('hidden');
 
   if (navItems.classList.contains('hidden')) {
-    navItems.style.height = '0vh';
-    navItems.style.overflow = 'hidden';
+    navItems.classList.remove('hidden');
+    navItems.style.maxHeight = navItems.scrollHeight + 'vh';
   } else {
-    navItems.style.height = '20vh';
-    navItems.style.overflow = 'visible';
+    navItems.removeAttribute('style');
+    navItems.classList.add('hidden');
   }
 });
 
 // Smooth Scrolling
 
-const smooth = document.querySelector('.nav-links');
+// const smooth = document.querySelector('.nav-items');
 
-smooth.addEventListener('click', function (e) {
+navItems.addEventListener('click', function (e) {
   e.preventDefault();
   console.log(e.target);
 
-  if (e.target.classList.contains('nav-link')) {
+  if (e.target.classList.contains('items')) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
 
 ////////////////////////////////////////////////////////////////
-
-// Form submit
-
-// const form = document.getElementById('my-form');
-// const btnsubmit = document.querySelector('.btnsubmit');
-
-// const sendData = async function () {
-//   const rec = await fetch('https://formspree.io/f/xlezeoyo'), {
-//     Method: 'Post', {
-
-//     }
-//   }
-//   console.log(rec);
-
-// };
-
-// sendData();
